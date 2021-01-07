d565f3ad3cc9a97f74f8b10f0c6d07981eb1493e
unknown <to2ktrung@gmail.com>
2021-01-08 02:03:23 +0700

    commit

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -138,32 +138,18 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{asset('public/frontend/img/about/team-1.jpg')}}" alt="">
-                        <h4>John Smith</h4>
-                        <span>Fashion Design</span>
+                        <img src="{{asset('public/frontend/img/about/thanh-trung.jpg')}}" alt="">
+                        <h4>Truong Thanh Trung</h4>
+                        <span>Developer fullstack</span>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{asset('public/frontend/img/about/team-2.jpg')}}" alt="">
-                        <h4>Christine Wise</h4>
+                        <img src="{{asset('public/frontend/img/about/sambath.jpg')}}" alt="">
+                        <h4>Sem SamBath</h4>
                         <span>C.E.O</span>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="team__item">
-                        <img src="{{asset('public/frontend/img/about/team-3.jpg')}}" alt="">
-                        <h4>Sean Robbins</h4>
-                        <span>Manager</span>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="team__item">
-                        <img src="{{asset('public/frontend/img/about/team-4.jpg')}}" alt="">
-                        <h4>Lucy Myers</h4>
-                        <span>Delivery</span>
                     </div>
                 </div>
             </div>
